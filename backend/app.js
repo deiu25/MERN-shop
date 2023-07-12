e7110280a0171e5f839ba55c471c94e9f36a0da2
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-12 12:58:01 +0300

    project setup

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+//Importing all routes
+const products = require('./routes/product');
+
+app.use('/api/v1', products);
+
+module.exports = app;
+
+//node backend/server.js
+//npm run dev
+//npm run prod
