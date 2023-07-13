ace48816e20ce20e36ba119e4d054dab3b227a2e
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-13 13:15:58 +0300

    create errors middleware

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const ErrorHandler = require('../utils/errorHandler');
 
 // Create new product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
@@ -28,11 +29,8 @@ exports.getSingleProduct = async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
     if (!product) {
-        return res.status(404).json({
-            status: false,
-            message: 'Product not found'
-        })
-    }
+        return next(new ErrorHandler('Product not found', 404));
+        }
 
     res.status(200).json({
         status: true,
@@ -74,7 +72,7 @@ exports.deleteProduct = async (req, res, next) => {
             message: 'Product not found'
         });
     }
-    
+
     await product.deleteOne();
 
     res.status(200).json({
