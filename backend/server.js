42b63d473558e7402b49d711eeff79676553d2d0
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-13 14:24:52 +0300

    validation &  Mongose ID error

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
 
+//Handle Uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down the server due to Uncaught Exception');
+    process.exit(1)
+})
+
 //Setting up config file
 dotenv.config({ path: 'backend/config/config.env' });
 
