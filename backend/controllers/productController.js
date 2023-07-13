0f8d3fa61f2a83b0cf6cfa6989402975169c2e03
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-13 12:14:36 +0300

    get product by its id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,9 +10,32 @@ exports.newProduct = async (req, res, next) => {
     })
 }
 
-exports.getProducts = (req, res, next) => {
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
+
     res.status(200).json({
         status: true,
-        message: 'This route will show all products in database'
+        count: products.length,
+        products
+    })
+}
+
+// Get single product details => /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            status: false,
+            message: 'Product not found'
+        })
+    }
+
+    res.status(200).json({
+        status: true,
+        product
     })
 }
