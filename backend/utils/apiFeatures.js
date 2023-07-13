661ecf74ee5074979222702970b2ef60f0b1420e
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-13 15:39:28 +0300

    filter and pagination

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -16,6 +16,33 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    //Filter
+    filter() {
+        const queryCopy = { ...this.queryString };
+
+        //Removing fields from the query
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        //Advance filter for price, ratings etc
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    //Pagination
+    pagination(resPerPage) {
+        const currentPage = Number(this.queryString.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resPerPage).skip(skip);
+        return this;
+    }
+
+
 }
 
 module.exports = APIFeatures;
