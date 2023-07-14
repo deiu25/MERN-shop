480066632412b6d9bc1a6b3fe4c00969ebb97c64
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-14 12:07:16 +0300

    generate forgot password token and send password recovery email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,7 +20,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route('/logout').get(logoutUser);
 
-// router.route('/password/forgot').post(forgotPassword);
+router.route('/password/forgot').post(forgotPassword);
 // router.route('/password/reset/:token').put(resetPassword);
 
 // router.route('/me').get(isAuthenticatedUser, getUserProfile);
