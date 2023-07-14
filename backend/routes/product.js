1061d1a10e0096995199f3a06fcffaff394e1a29
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-14 11:00:14 +0300

    add user in new product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -12,11 +12,11 @@ const {
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/products").get(getProducts);
-
 router.route("/product/:id").get(getSingleProduct);
 
-router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
-
-router.route("/admin/product/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct).delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router.route("/admin/product/new").post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
+router.route("/admin/product/:id")
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
 module.exports = router;
