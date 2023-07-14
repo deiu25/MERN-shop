d90ca419e7a75787959bea6fde9e10a3664b0ae4
deiu25 <deiu25@gmail.com>
2023-07-14 12:45:05 +0300

    change update/ change user logged password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -142,6 +142,23 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 }
 );
 
+// Update / Change password => /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select("+password");
+
+    // Check previous user password
+    const isMatched = await user.comparePassword(req.body.oldPassword);
+    if (!isMatched) {
+        return next(new ErrorHandler("Old password is incorrect", 400));
+    }
+
+    user.password = req.body.password;
+    await user.save();
+
+    sendToken(user, 200, res);
+}
+);
+
 // Get currently logged in user details => /api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
     const user = await User.findById(req.user.id);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -26,7 +26,7 @@ router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
-// router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 // router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
 // router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
