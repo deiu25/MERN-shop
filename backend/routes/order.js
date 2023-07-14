711b3ce9a3c6123889347fc907b5173a6b764481
deiu25 <deiu25@gmail.com>
2023-07-14 16:23:01 +0300

    delete order by id - admin

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { newOrder, getSingleOrder, myOrders, allOrders } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders, allOrders, updateOrder, deleteOrder } = require("../controllers/orderController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
@@ -12,5 +12,8 @@ router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
 router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), allOrders);
+
+router.route("/admin/order/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+                                .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
