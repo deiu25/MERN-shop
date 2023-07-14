21007dc4fc856d944a8e2c03eb7e7588870a089c
deiu25 <deiu25@gmail.com>
2023-07-14 15:17:38 +0300

    get single order, get logged in user orders, get all orders-admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,3 +33,50 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+// Get single order => /api/v1/order/:id
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("No Order found with this ID", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// Get logged in user orders => /api/v1/orders/me
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user.id });
+    
+    res.status(200).json({
+        success: true,
+        orders,
+    });
+    }
+);
+
+// Get all orders - ADMIN => /api/v1/admin/orders/
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find();
+    
+    let totalAmount = 0;
+
+    orders.forEach(order => {
+        totalAmount += order.totalPrice;
+    })
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders,
+    });
+    }
+);
+
