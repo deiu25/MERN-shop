982403c51cb9442e0a61ac2b82334fe8112171ae
deiu25 <deiu25@gmail.com>
2023-07-14 12:54:51 +0300

    update profile of logged in user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -159,6 +159,26 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
 }
 );
 
+// Update user profile => /api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+
+    // Update avatar: TODO
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+
 // Get currently logged in user details => /api/v1/me
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
     const user = await User.findById(req.user.id);
@@ -169,4 +189,5 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
     });
 }
 );
+
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,7 +27,7 @@ router.route('/password/reset/:token').put(resetPassword);
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
-// router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
 // router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
 // router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
