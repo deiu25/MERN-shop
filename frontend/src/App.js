6d588472bbde9c22847a1d3542ee6042411a4919
deiu25 <deiu25@gmail.com>
2023-07-17 13:57:46 +0300

    inst react and add head and foot comps

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Footer } from "./components/leyout/Footer";
+import { Header } from "./components/leyout/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
