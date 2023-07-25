c936fc80c67abe89b7617e25b7e1f22fc4f14a17
deiu25 <deiu25@gmail.com>
2023-07-25 10:47:40 +0300

    cart component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Home } from "./components/Home";
 import { Footer } from "./components/leyout/Footer";
 import { Header } from "./components/leyout/Header";
 import { ProductDetails } from "./components/product/ProductDetails";
+import { Cart } from "./components/cart/Cart";
 import { Login } from "./components/user/Login";
 import { Register } from "./components/user/Register";
 import { useEffect } from "react";
@@ -32,6 +33,7 @@ function App() {
             <Route path="/" element={<Home />}/>
             <Route path="/search/:keyword" element={<Home />}/>
             <Route path="/product/:id" element={<ProductDetails />}/>
+            <Route path="/cart/:id?" element={<Cart />}/>
             <Route path="/login" element={<Login />}/>
             <Route path="/register" element={<Register/>}/>
             <Route path="/me" element={<PrivateComponent element={Profile} />} />
