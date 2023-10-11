a6a781dba7e759a16828a4688d6cb80b11bef2ba
Badulescu Andrei <d3iu25@gmail.com>
2023-10-11 22:51:54 +0300

    nothing

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
+
+app.use(cors());
 
 // Importing all routes
 const products = require('./routes/product');
